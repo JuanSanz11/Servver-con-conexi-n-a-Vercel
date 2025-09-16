b9d783129c2f53cec4d52df47b8b2ae9fe94b4d7
Zodde <juandavidsanzalmanza@outlook.com>
2025-09-16 17:03:04 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const usersRouter = require('./users');
-const serverless = require('serverless-http');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Rutas
 app.use('/api/users', usersRouter);
 
+// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Servidor funcionando correctamente');
 });
 
-module.exports = serverless(app);
+// Puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+module.exports = app;
