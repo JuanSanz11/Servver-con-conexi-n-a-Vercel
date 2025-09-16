47e5c66f8463422e400277467810e865fe026bbb
Zodde <juandavidsanzalmanza@outlook.com>
2025-09-16 18:59:22 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // 👈 Esta línea debe ir arriba
 const usersRouter = require('./users');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Rutas
 app.use('/api/users', usersRouter);
 
-
-// Ruta de prueba
-const path = require('path');
-
+// Página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
